f18a344719283b6ce93570aeca714bd2b4f7b5db
Josef Waller <josef@siriusapplications.com>
2021-07-31 08:48:53 -0700

    Add linting libraries

---
+++ src/coord.py
@@ -0,0 +1,18 @@
+class Coord:
+	"""
+	A class used to represent the coordinate on the Catan board. Stores a coordinate on a triangular grid,
+	so that each hex and point both has a unique coord.
+	
+	Args:
+		q (int): The q coordinate
+		r (int): The r coordinate
+	"""
+	def __init__(self, q, r):
+		self.q = q
+		self.r = r
+	
+	def __hash__(self):
+		return hash((self.q, self.r))
+	
+	def __eq__(self, other):
+		return self.q == other.q and self.r == other.r
